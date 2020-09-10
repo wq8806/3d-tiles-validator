5b99028b606e3aa1b44d996b48b431bcea814960
wuzhongcheng <339005483@qq.com>
2020-09-10 16:02:03 +0800

    暂存

--- samples-generator/lib/readXml.ts
+++ samples-generator/lib/readXml.ts
@@ -3,6 +3,15 @@ import { Promise as Bluebird } from 'bluebird';
 var fsExtra = require('fs-extra');
 var fastXmlParser = require('fast-xml-parser');
 import {JSONPath} from 'jsonpath-plus';
+import {
+    Cartesian3,
+    defaultValue,
+    defined,
+    Math as CesiumMath,
+    Matrix4,
+    Matrix3,
+    Quaternion,
+} from 'cesium';
 
 let lengthUnit = 0;
 export function readXml(options) {
@@ -70,6 +79,9 @@ function traverseJson(obj,map) {
                 var minXYZStr = obj[key]['attr']['minXYZ'];
                 var maxSYXStr = obj[key]['attr']['maxXYZ'];
                 let placementStr = "";
+                if(key === "IfcProject"){ //不包含ObjectPlacement
+                    placementStr = obj[key]['IfcSite']['attr']['ObjectPlacement'];
+                }
                 if(obj[key]['attr'].hasOwnProperty("ObjectPlacement")){
                     placementStr = obj[key]['attr']['ObjectPlacement'];
                 }
@@ -91,11 +103,11 @@ function traverseJson(obj,map) {
     return map;
 }
 
-class Cartesian3 {
+/*class Cartesian3 {
     x:number = 0;
     y:number = 0;
     z:number = 0;
-};
+};*/
 
 export class ElementInfo {
     center: Cartesian3 = new Cartesian3();
@@ -107,29 +119,6 @@ export class ElementInfo {
 
 function computeAABB(minXYZStr,maxXYZStr,placementStr) {
     let elementInfo:ElementInfo = new ElementInfo();
-    var minXYZ = minXYZStr.split(' ');
-    var maxXYZ = maxXYZStr.split(' ');
-    for (let i = 0; i < 3; i++) {
-        minXYZ[i] = Number(minXYZ[i]);
-        maxXYZ[i] = Number(maxXYZ[i]);
-    }
-    let center:Cartesian3 = new Cartesian3();
-    center.x = (minXYZ[0] + maxXYZ[0]) / 2;    //Ifc中轴向与gltf有差异
-    center.y = (minXYZ[2] + maxXYZ[2]) / 2;
-    center.z = 0 - (minXYZ[1] + maxXYZ[1]) / 2;
-    // center.z = (minXYZ[1] + maxXYZ[1]) / 2;
-
-    let dimensions:Cartesian3 = new Cartesian3();
-    dimensions.x = maxXYZ[0] - minXYZ[0];
-    dimensions.y = maxXYZ[2] - minXYZ[2];
-    dimensions.z = maxXYZ[1] - minXYZ[1];
-
-    elementInfo.center = center;
-    elementInfo.dimensions = dimensions;
-    /*bounds.minXYZ = minXYZ;
-    bounds.maxXYZ = maxXYZ;*/
-    elementInfo.minXYZ = [center.x - dimensions.x / 2 ,center.y - dimensions.y / 2, center.z - dimensions.z/2];
-    elementInfo.maxXYZ = [center.x + dimensions.x / 2 ,center.y + dimensions.y/2, center.z + dimensions.z/2];
 
     //objectPlacement
     let placement = [];
@@ -157,6 +146,42 @@ function computeAABB(minXYZStr,maxXYZStr,placementStr) {
         placement[15] *= 1;
     }
     elementInfo.objectPlacement = placement;
-    
+
+    //minXYZStr maxXYZStr 是轴向对齐的，未考虑旋转，revit示例不适用
+    let minXYZ = minXYZStr.split(' ');
+    let maxXYZ = maxXYZStr.split(' ');
+    for (let i = 0; i < 3; i++) {
+        minXYZ[i] = Number(minXYZ[i]);
+        maxXYZ[i] = Number(maxXYZ[i]);
+    }
+    minXYZ = new Cartesian3(minXYZ[0],minXYZ[1],minXYZ[2]);
+    maxXYZ = new Cartesian3(maxXYZ[0],maxXYZ[1],maxXYZ[2]);
+
+    const mat4 = Matrix4.fromColumnMajorArray(placement);
+    const mat3 = Matrix4.getMatrix3(mat4,new Matrix3());
+    const rotation = Matrix3.getRotation(mat3,mat3);
+    const rotation_inverse = Matrix3.inverse(rotation,new Matrix3());
+
+    minXYZ = Matrix3.multiplyByVector(rotation,minXYZ,minXYZ);
+    maxXYZ = Matrix3.multiplyByVector(rotation,maxXYZ,maxXYZ);
+
+    let center:Cartesian3 = new Cartesian3();
+    center.x = (minXYZ.x + maxXYZ.x) / 2;    //Ifc中轴向与gltf有差异
+    center.y = (minXYZ.z + maxXYZ.z) / 2;
+    center.z = 0 - (minXYZ.y + maxXYZ.y) / 2;
+    // center.z = (minXYZ[1] + maxXYZ[1]) / 2;
+
+    let dimensions:Cartesian3 = new Cartesian3();
+    dimensions.x = maxXYZ.x - minXYZ.x;
+    dimensions.y = maxXYZ.z - minXYZ.z;
+    dimensions.z = maxXYZ.y - minXYZ.y;
+
+    elementInfo.center = center;
+    elementInfo.dimensions = dimensions;
+    /*bounds.minXYZ = minXYZ;
+    bounds.maxXYZ = maxXYZ;*/
+    elementInfo.minXYZ = [center.x - dimensions.x / 2 ,center.y - dimensions.y / 2, center.z - dimensions.z/2];
+    elementInfo.maxXYZ = [center.x + dimensions.x / 2 ,center.y + dimensions.y/2, center.z + dimensions.z/2];
+
     return elementInfo;
 }
